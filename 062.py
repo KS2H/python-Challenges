4cf19963fc7edef21b795414ef1fb6b990503a32
김세환 <115784029+KS2H@users.noreply.github.com>
2022-10-25 19:50:13 +0900

    :white_check_mark:upload file
    turtle graphic

---
+++ 062.py
@@ -0,0 +1,7 @@
+import turtle
+
+turtle.shape("turtle")
+
+for i in range(90):
+    turtle.forward(4)
+    turtle.right(4)
---
+++ 063.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.shape("turtle")
+
+for i in range(4): 
+    turtle.color("black")
+    turtle.pendown()
+    turtle.forward(100)
+    turtle.right(90)
+turtle.penup()
+turtle.forward(200)
+for i in range(4): 
+    turtle.color("red")
+    turtle.pendown()
+    turtle.forward(100)
+    turtle.right(90)
+turtle.penup()
+turtle.forward(200)
+for i in range(4): 
+    turtle.color("green")
+    turtle.pendown()
+    turtle.forward(100)
+    turtle.right(90)
