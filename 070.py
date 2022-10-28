7a85883b9aad8200f23039a73f8f2b15ac066457
김세환 <115784029+KS2H@users.noreply.github.com>
2022-10-28 18:51:10 +0900

    :white_check_mark:upload file

---
+++ 070.py
@@ -0,0 +1,5 @@
+country = ["Korea","Japan","China","France","England"]
+for i in country:
+    print(i)
+enter = int(input("Enter the number where the country is located in the list\n"))
+print(country[enter+-1])
