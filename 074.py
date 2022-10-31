04e9b119504914112ce55e6263624f441420ddfa
김세환 <115784029+KS2H@users.noreply.github.com>
2022-10-31 23:48:03 +0900

    :white_check_mark:upload file

---
+++ 074.py
@@ -0,0 +1,4 @@
+colorlist = ["black","yellow","green","white","blue","red","orange","pink","purple","light green"]
+answer1 = int(input("Please enter a number between 0 and 4"))
+answer2 = int(input("Please enter a number between 5 and 9"))
+print(colorlist[answer1:answer2+1])
