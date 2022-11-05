920f9d5d0bdca18a00c962c5434322aaa3a56c49
김세환 <115784029+KS2H@users.noreply.github.com>
2022-11-05 21:45:47 +0900

    :white_check_mark:upload file

---
+++ 080.py
@@ -0,0 +1,7 @@
+name = input("enter a name\n")
+print(len(name))
+firstname = input("enter a first name\n")
+print(len(firstname))
+print(firstname," ",name)
+name = firstname+" "+name
+print(len(name))
