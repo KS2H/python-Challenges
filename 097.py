4b745ebe8419d80c3f84fdd0127409d2c831a6f3
김세환 <115784029+KS2H@users.noreply.github.com>
2022-11-18 23:21:41 +0900

    ✅upload file

---
+++ 097.py
@@ -0,0 +1,5 @@
+simple_array = [[2,5,8],[3,7,4],[1,6,9],[4,2,0]]
+print(simple_array)
+line = int(input("Please enter a line : "))
+column = int(input("Please enter column : "))
+print(simple_array[line][column])
