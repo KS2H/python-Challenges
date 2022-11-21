f0241d7ab18ae99718fdf8a8eb6954f4d6c16351
김세환 <115784029+KS2H@users.noreply.github.com>
2022-11-21 21:16:33 +0900

    ✅upload file

---
+++ 103.py
@@ -0,0 +1,8 @@
+list = {}
+for i in range(4):
+    name = input("Please enter your name : ")
+    age = int(input("Please enter your age : "))
+    shoe = int(input("Please enter your shoe size : "))
+    list[name]={"Age":age,"Shoe_size":shoe}
+for name in list:
+    print((name),list[name]["Age"])
